90eb667efe89ebaf48b4f59d013ccf88e9c8faab
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-08-24 11:18:39 +0200

    add: contact page

---
+++ pages/contact.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <p>
+            Here are the mandatory social media links.
+            It's random stuff mostly, but sometimes I post about current projects.
+        </p>
+
+        <div class="flex flex-col items-start gap-3 my-12 ml-6 text-3xl uppercase">
+            <BrandGradient class="flex">
+                <a href="https://instagram.com/kasper.jha">instagram</a>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5 mt-1 ml-1">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+                </svg>
+            </BrandGradient>
+            <BrandGradient class="flex">
+                <a href="https://https://twitter.com/kasperjha">twitter</a>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5 mt-1 ml-1">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+                </svg>
+            </BrandGradient>
+        </div>
+
+        <p>
+            Want to do a project with me or just discuss DIY electronics?
+        </p>
+
+        <p class="mt-6 ml-6">
+            Email me anytime at <br>
+            <a class="underline" href="mailto:kakka0903@hotmail.no">kakka0903@hotmail.no</a>
+        </p>
+
+    </div>
+</template>
