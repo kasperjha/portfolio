163ea95a26fda548cd7ca3af10a335b95146cbfd
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-08-24 11:18:27 +0200

    add: landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,14 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1 class="mt-16 text-6xl leading-tight ">Heyo!<br>I'm Kasper </h1>
+    <p class="mt-6 text-lg" >I am a developer and designer from Oslo. Besides studying for a CompSci degree I create cool websites for cool people.</p>
+    <div class="flex flex-col items-end gap-3 mt-12 text-4xl">
+      <BrandGradient class="transition hover:-translate-x-4">
+        <NuxtLink to="/contact">CONTACT</NuxtLink>
+      </BrandGradient>
+      <BrandGradient class="transition hover:-translate-x-4">
+        <NuxtLink to="/work" >WORK</NuxtLink>
+      </BrandGradient>
+    </div>
+  </div>
 </template>
