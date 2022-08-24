c04af4197a31c03a08a2e39d6eae6953ac80824a
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-08-24 15:20:17 +0200

    add: remove head

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,20 +11,4 @@ export default defineNuxtConfig({
         apiHost:'https://plausible.kakka.dev'
     },
 
-    // Global page headers: https://go.nuxtjs.dev/config-head
-    head: {
-        title: 'kakka.dev',
-        htmlAttrs: {
-            lang: 'en'
-        },
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' },
-            { name: 'format-detection', content: 'telephone=no' }
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-        ]
-    },
 })
