e8e94599a9cd8c156916425f3546328ab857ee37
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-08-24 13:31:30 +0200

    refactor: resposive + style

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,19 @@
 <template>
-  <div>
-    <h1 class="mt-16 text-6xl leading-tight ">Heyo!<br>I'm Kasper </h1>
-    <p class="mt-6 text-lg" >I am a developer and designer from Oslo. Besides studying for a CompSci degree I create cool websites for cool people.</p>
-    <div class="flex flex-col items-end gap-3 mt-12 text-4xl">
+  <div class="space-y-10">
+
+    <h1 class="pt-12 text-6xl leading-tight">
+      <span class="italic font-bold">Heyo!</span><br>
+      I'm Kasper
+    </h1>
+
+    <p class="max-w-lg">
+      I am a developer and designer from Oslo.
+      During daytime i study for a CompSci degree in NL, but on the side i enjoy building
+      cool websites for cool people.
+    </p>
+
+
+    <div class="flex flex-col items-end gap-3 text-4xl">
       <BrandGradient class="transition hover:-translate-x-4">
         <NuxtLink to="/contact">CONTACT</NuxtLink>
       </BrandGradient>
