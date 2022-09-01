d0c94c8397d7b705687f049318c4a3be16ec5193
Kasper Johannes Hopen Alfarnes <kakka0903@hotmail.no>
2022-09-01 12:37:28 +0200

    change domain for analytics

--- app.vue
+++ app.vue
@@ -24,7 +24,7 @@
 useHead({
     title:'kakka.dev',
     script:[
-        { defer:'defer', src:'https://plausible.kakka.dev/js/plausible.js', 'data-domain':'kakka.dev'}
+        { defer:'defer', src:'https://plausible.kakk.dev/js/plausible.js', 'data-domain':'kakka.dev'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }
