728c287fab418674af069faa39e114e9d9cb2fd1
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2022-11-16 21:56:41 +0100

    add: TextLink component

---
+++ components/TextLink.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+    link:String,
+})
+
+</script>
+
+<template>
+    <NuxtLink :to="link" class="hover:text-orange-700 underline">
+        <slot/>
+    </NuxtLink>
+</template>
