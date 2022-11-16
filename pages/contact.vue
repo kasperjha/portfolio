f0201af9f23b80336d9bb0aac94ef319c11677ff
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2022-11-16 22:14:44 +0100

    add: github link

--- pages/contact.vue
+++ pages/contact.vue
@@ -13,7 +13,13 @@
                 </svg>
             </BrandGradient>
             <BrandGradient class="flex tranform transition hover:translate-x-4">
-                <a href="https://https://twitter.com/kasperjha">twitter</a>
+                <a href="https://twitter.com/kasperjha">twitter</a>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5 mt-1 ml-1">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+                </svg>
+            </BrandGradient>
+            <BrandGradient class="flex tranform transition hover:translate-x-4">
+                <a href="https://github.com/kakka0903">github</a>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5 mt-1 ml-1">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
                 </svg>
