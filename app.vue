0ca9c4957ad2e4f7f7432eb99a9d9b9330dd5da9
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2022-11-16 21:20:56 +0100

    add: center page content

--- app.vue
+++ app.vue
@@ -1,23 +1,25 @@
 <template>
-    <section class="flex flex-col min-h-screen px-8 font-mono text-gray-900">
-        <nav class="flex w-full pt-6 pb-4 gap-x-4">
-            <NuxtLink class="flex items-center font-bold" to="/">
-                kakka.dev
-            </NuxtLink>
-            <NuxtLink class="ml-auto" to="/contact">contact</NuxtLink>
-            <NuxtLink to="/work">work</NuxtLink>
-        </nav>
-        <main class="flex-grow py-5">
-            <NuxtPage />
-        </main>
-        <footer class="flex justify-center py-4 text-xs italic text-gray-700">
-            <p>
-                developed with <a class="underline" href="https://nuxtjs.org">nuxt</a>,
-                <a class="underline" href="https://vuejs.org/">vue</a> and
-                <a class="underline" href="https://tailwindcss.com">tailwind</a>
-            </p>
-        </footer>
-    </section>
+    <div class="flex w-full justify-center bg-gray-100">
+        <section class="max-w-xl w-full flex flex-col min-h-screen px-8 font-mono text-gray-900">
+            <nav class="flex w-full pt-6 pb-4 gap-x-4">
+                <NuxtLink class="flex items-center font-bold" to="/">
+                    kakka.dev
+                </NuxtLink>
+                <NuxtLink class="ml-auto" to="/contact">contact</NuxtLink>
+                <NuxtLink to="/work">work</NuxtLink>
+            </nav>
+            <main class="flex-grow py-5">
+                <NuxtPage />
+            </main>
+            <footer class="flex justify-center py-4 text-xs italic text-gray-700">
+                <p>
+                    developed with <a class="underline" href="https://nuxtjs.org">nuxt</a>,
+                    <a class="underline" href="https://vuejs.org/">vue</a> and
+                    <a class="underline" href="https://tailwindcss.com">tailwind</a>
+                </p>
+            </footer>
+        </section>
+    </div>
 </template>
 
 <script setup>
