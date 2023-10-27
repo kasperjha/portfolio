7472961bcd08c9a5a054b1587f87428d1509bebd
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2023-10-28 00:26:30 +0200

    update: socials

--- pages/contact.vue
+++ pages/contact.vue
@@ -12,13 +12,7 @@
                 </svg>
             </BrandGradient>
             <BrandGradient class="transition ease-in-out duration-300 hover:translate-x-4 flex">
-                <a href="https://twitter.com/kasperjha">twitter</a>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5 mt-1 ml-1">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
-                </svg>
-            </BrandGradient>
-            <BrandGradient class="transition ease-in-out duration-300 hover:translate-x-4 flex">
-                <a href="https://github.com/kakka0903">github</a>
+                <a href="https://www.linkedin.com/in/kasper-alfarnes-a3b97b25b/">linkedin</a>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5 mt-1 ml-1">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
                 </svg>
