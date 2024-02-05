35f6a3aae448dd17a9fd30faa112b553fcd7acb8
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2024-02-05 23:25:35 +0100

    add: project card component

---
+++ components/ProjectCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="min-h-[75px] rounded-lg border bg-gray-200 border-gray-800 hover:bg-blue-200 flex p-4 gap-6 transition">
+    <div class="flex flex-col justify-center gap-1" >
+      <p class="text-xl font-bold">{{ project.title }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Project } from '../types/Projects';
+defineEmits(['selectedProject'])
+defineProps<{project: Project}>()
+</script>
---
+++ types/Projects.ts
@@ -0,0 +1,6 @@
+export interface Project {
+  title: string,
+  short_description: string,
+  visit_url?: string,
+  github_url?: string,
+}
