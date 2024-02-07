399d9a24fc75dd6f36132f8179e7680346bc78f5
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2024-02-07 12:17:38 +0100

    refactor: change description wording

--- data/projects.ts
+++ data/projects.ts
@@ -3,23 +3,23 @@ import { Project } from "../types/Projects";
 export const projects: Project[] = [
   {
     title: 'ZvanzMP3 V2',
-    short_description: 'Second iteration of a website promoting and selling beats produced by ZvanzMP3. Built with Vue2, Flask, Tailwind, Stripe and Paypal.',
+    short_description: 'Second iteration of a website promoting and selling the work of ZvanzMP3. Built with Vue, Flask and Tailwind.',
     visit_url: 'https://zvanz-frontend.pages.dev'
   },
   {
     title: 'orkide.world',
-    short_description: "A novel way of presenting music videos and album covers made by Orkidé Media. Built with Vue, Nuxt and Tailwind. Powered by self-hosted Strapi and Plausible instances.",
+    short_description: "Portfolio website promoting the work of Orkidé Media. Built with Vue, Nuxt and Tailwind.",
     visit_url: 'https://orkide.world'
   },
   {
     title: 'delivery-data',
-    short_description: 'POC for a web application that enables tracking of package wear, deliverer performance and technical statistics from sensors. Team project for a CS course at RuG in the Netherlands.',
+    short_description: 'POC for a web application for tracking package wear, deliverer performance and sensor data. Built with Vue, Nuxt and Tailwind.',
     github_url: 'https://github.com/kakka0903/delivery-data',
     visit_url: 'https://delivery-data.pages.dev/'
   },
   {
     title: 'kakka.dev',
-    short_description: 'This website. Personal portfolio for web development projects. Built with Nuxt, Vue and Tailwind.',
+    short_description: 'Personal portfolio website for demonstrating past web development projects. Built with Nuxt, Vue and Tailwind.',
     github_url: 'https://github.com/kakka0903/kakka.dev',
   }
 ]
