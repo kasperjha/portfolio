acb0f380cc4741076ed68ab816e49b8bb1c5229b
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2024-02-07 11:58:04 +0100

    add: zvanzmp3 v2 project

--- data/projects.ts
+++ data/projects.ts
@@ -2,6 +2,11 @@ import { Project } from "../types/Projects";
 
 export const projects: Project[] = [
   {
+    title: 'ZvanzMP3 V2',
+    short_description: 'Second iteration of a website promoting and selling beats produced by ZvanzMP3. Built with Vue2, Flask, Tailwind, Stripe and Paypal.',
+    visit_url: 'https://zvanz-frontend.pages.dev'
+  },
+  {
     title: 'orkide.world',
     short_description: "A novel way of presenting music videos and album covers made by Orkidé Media. The application is built with Vue, Nuxt and Tailwind. It's integrated with selfhosted Strapi CMS and Plausible instances.",
     github_url: 'https://github.com/kakka0903/orkide-frontend',
