c27ddcda39986cd1983f8bee4a3bf52039da96f8
Kasper Johannes Hopen Alfarnes <49387825+kakka0903@users.noreply.github.com>
2024-02-26 15:25:58 +0100

    Landing page redesign (#8)
    * add: portrait in hero
    
    * refactor: change design of CTA links
    
    * add: h2 headings and change text content
    
    * fix: remove spacing
    
    * fix: reorder content
    
    * fix: update text content

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,44 @@
 <template>
-  <div class="space-y-10">
+  <div>
 
-    <h1 class="text-5xl leading-tight">
-      <span class="italic font-bold">Heyo!</span><br>
-      I'm Kasper
-    </h1>
-
-    <p class="max-w-lg">
-      I am a freelance web developer and designer from Oslo.
-      Currently I study Computing Science at Rijksuniversiteit Groningen,
-      and make websites in my spare time.
-    </p>
-
-
-    <div class="flex flex-col gap-3 items-end text-3xl">
-      <BrandGradient class="transition ease-in-out duration-300 hover:-translate-x-4">
-        <NuxtLink to="/contact">CONTACT</NuxtLink>
-      </BrandGradient>
-      <BrandGradient class="transition ease-in-out duration-300 hover:-translate-x-4">
-        <NuxtLink to="/work">WORK</NuxtLink>
-      </BrandGradient>
+    <div class="flex gap-10 items-center">
+      <img src="/me.jpeg" alt="" class="min-w-40 w-40 h-40 rounded-full border-[5px]">
+      <div class="space-y-2">
+        <h1 class="text-2xl font-bold">kas·per</h1>
+        <p>“Welcome! I'm a freelance web developer and soon to be CS graduate” </p>
+      </div>
     </div>
 
-    <p class="max-w-lg">
-      Most recently I built a portfolio for Orkidé.
-      He is a music video producer for rap groups in Oslo.
-      The website has a untraditional colorway and bold design. <text-link link="https://orkide.world">Check it out!</text-link>
+    <div class="flex gap-3 mt-8 justify-around text-3xl">
+      <div class="transition p-5 ease-in-out duration-300 hover:scale-125">
+        <BrandGradient>
+          <NuxtLink to="/contact">CONTACT</NuxtLink>
+        </BrandGradient>
+      </div>
+      <div class="transition p-5 ease-in-out duration-300 hover:scale-125">
+        <BrandGradient>
+          <NuxtLink to="/work">WORK</NuxtLink>
+        </BrandGradient>
+      </div>
+    </div>
+
+    <h2 class="font-bold mt-10">Availability</h2>
+
+    <p class="max-w-lg mt-2">
+      I'm open to work fulltime on a freelance project from September 2024.
+      Do you have a something for me? <text-link link="/contact">Reach out!</text-link>
+    </p>
+
+    <h2 class="font-bold mt-10">Recent Work</h2>
+
+    <p class="max-w-lg mt-2">
+      Recently I extended Orkidé's digital portfolio.
+      I added a folder archive, darkmode and improved
+      the overall user experience.
+    </p>
+
+    <p class="mt-2">
+      <text-link link="https://orkide.world">Check it out!</text-link>
     </p>
   </div>
 </template>
