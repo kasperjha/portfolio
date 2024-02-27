7a72737ed5422ae968e78bf83111b78ed81fee46
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2024-02-27 12:53:04 +0100

    fix: update contact info

--- pages/contact.vue
+++ pages/contact.vue
@@ -4,12 +4,18 @@
         <h1 class="text-3xl font-bold py-2">Contact</h1>
 
         <p>
-            Follow me here for the occasional webdev related post:
+            For ongoing projects, inspiration, and career updates follow me on these platforms:
         </p>
 
         <div class="flex flex-col items-start gap-3 py-5 ml-5 text-3xl uppercase">
             <BrandGradient class="transition ease-in-out duration-300 hover:translate-x-4 flex">
-                <a href="https://instagram.com/kasper.jha">instagram</a>
+                <a href="https://instagram.com/kasperkode">instagram</a>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5 mt-1 ml-1">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+                </svg>
+            </BrandGradient>
+            <BrandGradient class="transition ease-in-out duration-300 hover:translate-x-4 flex">
+                <a href="https://pinterest.com/kasperjha">pinterest</a>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5 mt-1 ml-1">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
                 </svg>
@@ -23,14 +29,10 @@
         </div>
 
         <p>
-            Do you know how to ride a bike without hands? or want a website?
+            Would you like to start a project together?
+            Shoot me an email at:
+            <text-link link="mailto:kakka0903@hotmail.no">kakka0903@hotmail.no</text-link>
         </p>
-
-        <p class="py-5 ml-5">
-            Tell me about it at: <br>
-            <a class="underline" href="mailto:kakka0903@hotmail.no">kakka0903@hotmail.no</a>
-        </p>
-
     </div>
 </template>
 
