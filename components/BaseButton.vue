b7c0c0a49afadf2be9c47e3e37ac8d15f102d6f7
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2024-03-19 04:09:36 +0100

    add: base button component

---
+++ components/BaseButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button
+    class="py-2 px-4 border-2 border-purple-600 rounded-md"
+    :class="variantStyles"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+const props = withDefaults(defineProps<{variant: 'primary' | 'secondary'}>(), {
+  variant: 'primary'
+});
+
+const variantStyles = computed(() => ({
+  'bg-purple-600 text-white hover:brightness-95': props.variant == 'primary',
+  'text-purple-600 bg-white hover:brightness-95': props.variant == 'secondary'
+}))
+</script>
