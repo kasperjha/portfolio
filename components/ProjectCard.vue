96d1e2ad91aae10b057ce2a335dcb06c30ea0a56
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2024-04-03 11:16:13 +0200

    refactor: new project card design

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,12 +1,12 @@
 <template>
   <div
-    class="cursor-pointer min-h-[75px] border hover:bg-blue-100 flex items-center p-4 gap-6 transition"
-    :class="{ 'bg-white border-gray-800': !selected, 'border-blue-800 bg-blue-100': selected,  }"
+    class="cursor-pointer rounded-md min-h-[75px] border hover:bg-purple-200/50 flex items-center p-4 gap-6 transition"
+    :class="{'bg-gray-100/50 border-gray-200 text-gray-800': !selected, 'bg-purple-200/50 border-purple-200 text-purple-600': selected}"
   >
     <ProjectIcon :palette="project.project_palette">
       {{ project.title.slice(0, 2) }}
     </ProjectIcon>
-    <p class="text-xl font-bold">{{ project.title }}</p>
+    <p class="text-xl font-medium">{{ project.title }}</p>
   </div>
 </template>
 
