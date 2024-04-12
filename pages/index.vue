b2f70410d018cd052ed763a3a24870926394d91f
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2024-04-12 15:10:14 +0200

    refactor: extract social links component

--- pages/index.vue
+++ pages/index.vue
@@ -53,28 +53,7 @@
         Follow me on social media to stay up to
         date on what I'm currently working on.
       </p>
-      <div class="mt-8 flex col justify-center gap-8">
-        <NuxtLink to="https://instagram.com/kasperkode">
-          <BaseCard>
-            <IconInstagram class="w-10 h-10 text-purple-600"/>
-          </BaseCard>
-        </NuxtLink>
-        <NuxtLink to="https://pinterest.com/kasperjha">
-          <BaseCard>
-            <IconPinterest class="w-10 h-10 text-purple-600"/>
-          </BaseCard>
-        </NuxtLink>
-        <NuxtLink to="https://x.com/kasperjha">
-          <BaseCard>
-            <IconX class="w-10 h-10 text-purple-600"/>
-          </BaseCard>
-        </NuxtLink>
-        <NuxtLink to="https://www.linkedin.com/in/kasper-alfarnes-a3b97b25b/">
-          <BaseCard>
-            <IconLinkedIn class="w-10 h-10 text-purple-600"/>
-          </BaseCard>
-        </NuxtLink>
-      </div>
+      <SocialsDisplay class="mt-8"/>
     </section>
   </div>
 </template>
