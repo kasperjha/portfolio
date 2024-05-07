b25ad265c4374d4952eb6c17b88d608bc45a57a4
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2024-05-07 23:51:54 +0200

    add: new latest entry

--- data/latest.ts
+++ data/latest.ts
@@ -1,6 +1,7 @@
 import { LatestEntry } from "../types/LatestEntry";
 
 export const latest: LatestEntry[]  = [
+  { date: '2024-05-04', content: 'Installing a self-hosted nextcloud instance'},
   { date: '2024-04-11', content: 'Looking for a job in the industry👀'},
   { date: '2024-04-01', content: 'Preparing for Operating Systems and Computer Graphics exams.'},
   { date: '2024-03-19', content: 'Pitched Local Bloom for Innovasjon Norge'}
