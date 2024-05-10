fec40c5a561d470b869d82494dda790492bc4f71
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2024-05-10 12:49:17 +0200

    refactor: update latest latest entry

--- data/latest.ts
+++ data/latest.ts
@@ -1,7 +1,7 @@
 import { LatestEntry } from "../types/LatestEntry";
 
 export const latest: LatestEntry[]  = [
-  { date: '2024-05-09', content: 'Writing a thesis on IoT security'},
+  { date: '2024-05-09', content: 'Writing my BSc. thesis on "Hardware security in IoT"'},
   { date: '2024-05-04', content: 'Installing a self-hosted nextcloud instance'},
   { date: '2024-04-11', content: 'Looking for a job in the industry👀'},
   { date: '2024-04-01', content: 'Preparing for Operating Systems and Computer Graphics exams.'},
