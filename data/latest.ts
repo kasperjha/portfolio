0c1d0b66276edb4650a4e40c850deb64d1a0d676
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2024-05-22 23:31:44 +0200

    add: new latest entry

--- data/latest.ts
+++ data/latest.ts
@@ -1,10 +1,11 @@
 import { LatestEntry } from "../types/LatestEntry";
 
-export const latest: LatestEntry[]  = [
-  { date: '2024-05-11', content: 'Hacking on a sideproject with realtime multiplayer pointer movement'},
-  { date: '2024-05-09', content: 'Writing my BSc. thesis on "Hardware security in IoT"'},
-  { date: '2024-05-04', content: 'Installing a self-hosted nextcloud instance'},
-  { date: '2024-04-11', content: 'Looking for a job in the industry👀'},
-  { date: '2024-04-01', content: 'Preparing for Operating Systems and Computer Graphics exams.'},
-  { date: '2024-03-19', content: 'Pitched Local Bloom for Innovasjon Norge'}
+export const latest: LatestEntry[] = [
+  { date: '2024-05-22', content: 'Changing my GitHub username...' },
+  { date: '2024-05-11', content: 'Hacking on a sideproject with realtime multiplayer pointer movement' },
+  { date: '2024-05-09', content: 'Writing my BSc. thesis on "Hardware security in IoT"' },
+  { date: '2024-05-04', content: 'Installing a self-hosted nextcloud instance' },
+  { date: '2024-04-11', content: 'Looking for a job in the industry👀' },
+  { date: '2024-04-01', content: 'Preparing for Operating Systems and Computer Graphics exams.' },
+  { date: '2024-03-19', content: 'Pitched Local Bloom for Innovasjon Norge' }
 ]
