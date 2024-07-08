204abe32bb2457f2ab62ddbf0391224a834f2a5b
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2024-07-08 23:39:28 +0200

    chore(deps): add nuxt content

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,9 @@
 export default defineNuxtConfig({
-    target: 'static',
-    mode: 'universal',
-    modules: ['@nuxtjs/tailwindcss'],
-    app: {
-        pageTransition: { name: 'page', mode: 'out-in', appear: true }
-    },
+  target: 'static',
+  mode: 'universal',
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/content'],
+  app: {
+      pageTransition: { name: 'page', mode: 'out-in', appear: true }
+  },
+  compatibilityDate: '2024-07-08',
 })
