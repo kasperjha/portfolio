89144958fa1455d7b367986af7c7fe157b20314b
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2024-07-09 17:32:34 +0200

    refactor: change look of landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,58 @@
 <template>
   <div>
-
     <NuxtLink to="/now">
       <latest class="mb-14" />
     </NuxtLink>
 
     <div class="flex flex-col items-center text-center gap-5">
       <img src="/me.jpeg" alt="" class="min-w-40 h-40 w-40 rounded-full border-[5px]">
-      <div class="space-y-4 max-w-sm">
-        <h1 class="text-2xl font-bold">kas·per</h1>
-        <p class="text-md">Hi there! I'm a freelance software developer, web designer and soon to be Computer Science graduate.</p>
-        <div class="space-x-4 pt-2 text-sm">
-          <NuxtLink to="/work">
-            <BaseButton>
-              See Projects
-            </BaseButton>
-          </NuxtLink>
-          <NuxtLink to="/blog">
-            <BaseButton variant="secondary">
-              See blogposts
-            </BaseButton>
-          </NuxtLink>
-        </div>
+      <div class="space-y-4 max-w-sm flex flex-col items-center">
+        <h1 class="text-2xl font-bold">Kasper JH Alfarnes</h1>
+        <p class="text-md">Hi there! I'm a freelance software developer, web designer and soon to be Computer Science
+          graduate.</p>
       </div>
     </div>
 
-    <h2 class="text-xl font-bold mt-16">About Me</h2>
+    <section class="mt-32 text-gray-600 space-y-2">
+      <h2 class="text-black">WHAT I DO</h2>
+      <p class="pb-4">
+        I'm interested in the intersection of design and technology.
+        Building for the web strikes a perfect balance, so I design and develop on freelance basis.
+      </p>
 
-    <p class="mt-2">
-      I'm a passionate developer, with an eye for design, great academic merits and industry experience from both large and small companies. (<NuxtLink to="https://www.linkedin.com/in/kasper-alfarnes-a3b97b25b/">see LinkedIn!</NuxtLink>)
-    </p>
+      <BaseButtonGroup>
+        <BaseButton variant="primary">
+          Work with me
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" class="size-5">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+          </svg>
+        </BaseButton>
 
-    <h2 class="text-xl font-bold mt-16">My Services</h2>
+        <BaseButton variant="secondary">
+          My projects
+        </BaseButton>
+      </BaseButtonGroup>
+    </section>
 
-    <p class="mt-2">
-      As a freelancer, I offer web development and brand identity services. I especially enjoy creating highly-interactive and memorable digital experiences.
-    </p>
+    <section class="mt-32 text-gray-600 space-y-2">
+      <h2 class="text-black">RECENT WRITING</h2>
+      <p class="pb-6">
+        Once in a while, I write about technology, design and ongoing projects.
+        Here is my latest post:
+      </p>
+      <BlogPostCard :post="newestPost" />
+    </section>
 
-    <h2 class="text-xl font-bold mt-16">Recent Work</h2>
+    <section class="mt-32 text-gray-600 space-y-2">
+      <h2 class="text-black">STAY IN TOUCH</h2>
+      <p class="pb-8">
+        If you want to stay up to date on what im doing you can follow me here
+      </p>
+      <SocialsDisplay />
+    </section>
 
-    <p class="mt-2">
-      Most recently I built <a href="https://kalf.no">kalf.no</a>
-      It's a fun little site with one line drawing made by friends.
-    </p>
-
-    <section class="flex flex-col items-center mt-20">
+    <!-- <section class="flex flex-col items-center mt-20">
       <h2 class="text-xl font-bold text-center">
         Wanna stay in touch?
       </h2>
@@ -52,7 +60,11 @@
         Follow me on social media to stay up to
         date on what I'm currently working on.
       </p>
-      <SocialsDisplay class="mt-8"/>
-    </section>
+    </section> -->
+
   </div>
 </template>
+
+<script lang="ts" setup>
+const newestPost = await queryContent().sort({ published: -1 }).findOne()
+</script>
