69ce6ed12bd8a2e3dc840d608764cfb5ccad558a
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2024-07-09 20:13:11 +0200

    feat: blog page description

--- pages/blog.vue
+++ pages/blog.vue
@@ -4,6 +4,11 @@
       Blog
     </BasePageTitle>
 
+    <p class="mb-8">
+      Once in a while I write about software, design and ongoing projects.
+      Please do reach out if you have any thoughts, comments or suggestions.
+    </p>
+
     <section class="space-y-4">
       <BlogPostCard v-for="post in data" :post="post" />
     </section>
@@ -12,4 +17,10 @@
 
 <script setup>
 const data = await queryContent('posts').find();
+
+useSeoMeta({
+  title: 'kakka.dev - blog',
+  description: 'Once in a while I write about software, design and ongoing projects. Please do reach out if you have any thoughts, comments or suggestions.'
+})
+
 </script>
