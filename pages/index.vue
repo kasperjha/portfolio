af85a7016833a9b8a54ff7f6057a94ab7d6bc0dd
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2024-07-09 20:15:41 +0200

    fix: add missing links

--- pages/index.vue
+++ pages/index.vue
@@ -21,17 +21,23 @@
       </p>
 
       <BaseButtonGroup>
-        <BaseButton variant="primary">
-          Work with me
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-            stroke="currentColor" class="size-5">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
-          </svg>
-        </BaseButton>
 
-        <BaseButton variant="secondary">
-          My projects
-        </BaseButton>
+
+        <NuxtLink to="/contact">
+          <BaseButton variant="primary">
+            Work with me
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="size-5">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+            </svg>
+          </BaseButton>
+        </NuxtLink>
+
+        <NuxtLink to="/work">
+          <BaseButton variant="secondary">
+            My projects
+          </BaseButton>
+        </NuxtLink>
       </BaseButtonGroup>
     </section>
 
