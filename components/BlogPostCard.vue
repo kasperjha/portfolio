a5d0637c81434b76080dfbfc6b3aad3fe8d01b98
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2024-07-10 11:46:51 +0200

    feat: small blog card variant

--- components/BlogPostCard.vue
+++ components/BlogPostCard.vue
@@ -2,7 +2,7 @@
   <NuxtLink :to="`/posts/${post.slug}`" class="block">
     <article class="rounded-lg p-6 border hover:bg-purple-200/50 border-gray-200 bg-gray-100/50 transition">
       <h2 class="mb-2 text-md text-black">{{ post.title }}</h2>
-      <p class="text-sm text-gray-600 mb-6">
+      <p class="text-sm text-gray-600 mb-6" v-if="variant !== 'small'">
         {{ post.description }}
       </p>
 
@@ -21,6 +21,10 @@ import { Post } from '../types/Post';
 
 interface Props {
   post: Post
+  variant: 'default' | 'small'
 }
-const props = defineProps<Props>()
+
+const props = withDefaults(defineProps<Props>(), {
+  variant: 'default'
+})
 </script>
