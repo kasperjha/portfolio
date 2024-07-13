f07125f8c627a648f75f03dd1267bb629c102d1e
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2024-07-13 04:59:29 +0200

    feat: add sitemap

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,16 @@
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/content', "@nuxt/image"],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/content', "@nuxt/image", "@nuxtjs/sitemap"],
   app: {
     pageTransition: { name: 'page', mode: 'out-in', appear: true }
+  },
+  site: {
+    url: 'https://alfarnes.dev',
+    trailingSlash: true,
+  },
+  sitemap: {
+    sources: [
+      '/api/__sitemap__/urls'
+    ]
   },
   compatibilityDate: '2024-07-08'
 })
---
+++ server/api/__sitemap__/urls.ts
@@ -0,0 +1,15 @@
+import type { ParsedContent } from '@nuxt/content/dist/runtime/types'
+import { serverQueryContent } from '#content/server'
+import { asSitemapUrl, defineSitemapEventHandler } from '#imports'
+
+// defines sitemap urls for blogposts
+export default defineSitemapEventHandler(async (e) => {
+  const contentList = (await serverQueryContent(e).find()) as ParsedContent[]
+  const urls = contentList
+    .filter(c => c._dir == 'posts')
+    .map(c => asSitemapUrl({
+      loc: `${c.slug}`,
+      lastmod: c.published
+    }))
+  return urls;
+})
