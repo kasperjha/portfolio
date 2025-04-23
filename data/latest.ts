18fc46bb57147925618422504cff45276de0086e
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-04-23 12:40:03 +0200

    feat: update latest entries

--- data/latest.ts
+++ data/latest.ts
@@ -1,8 +1,14 @@
 import { LatestEntry } from "../types/LatestEntry";
 
 export const latest: LatestEntry[] = [
-  { date: '2024-09-30', content: 'Presented my BSc thesis' },
-  { date: '2024-07-01', content: 'First day at new job' },
+  { date: '2025-04-23', content: 'Announcing Klosure (see LinkedIn).' },
+  { date: '2025-04-07', content: 'Automating infrastructure with Ansible' },
+  { date: '2025-04-02', content: "Renting a VPS at Hetzner Cloud" },
+  { date: '2025-01-20', content: 'Finish the last course of my BSC.' },
+  { date: '2024-12-20', content: 'Opened the annual minecraft server' },
+  { date: '2024-12-11', content: 'Travelled to Romania' },
+  { date: '2024-10-16', content: 'Defended my BSc thesis' },
+  { date: '2024-07-01', content: 'First day at LoanLink' },
   { date: '2024-05-22', content: 'Changing my GitHub username...' },
   { date: '2024-05-11', content: 'Hacking on a sideproject with realtime multiplayer pointer movement' },
   { date: '2024-05-09', content: 'Writing my BSc. thesis on "Hardware security in IoT"' },
