d09d04ebd6c7a963fcb46338949c78b4830d8d93
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-05-26 23:46:05 +0200

    fix: load latest from CMS

--- frontend/components/latest.vue
+++ frontend/components/latest.vue
@@ -1,14 +1,13 @@
 <template>
-  <button
+  <button v-if="latest"
     class="group text-xs w-full bg-purple-100/40 hover:brightness-95 border border-purple-200 py-2 px-4 rounded-full text-purple-600 flex items-center">
     <BoltIcon class="size-3 inline animate-pulse mr-1" />
-    <!-- TODO: update to use strapi -->
     <!-- TODO: remove hardcoded latest entries -->
     <p class="mr-3 animate-pulse">
-      {{ latest[0].date }}
+      {{ latest.date }}
     </p>
     <p class="overflow-hidden truncate text-ellipsis">
-      {{ latest[0].content }}
+      <StrapiBlocksText :nodes="latest.content" />
     </p>
   </button>
 </template>
@@ -16,4 +15,11 @@
 <script setup lang="ts">
 import { BoltIcon } from '@heroicons/vue/16/solid';
 import { latest } from '~/data/latest';
+
+const strapi = useStrapi()
+const latest = ref<LatestEntry | undefined>(undefined);
+
+strapi.find('latest-updates', { sort: 'date:desc', populate: '*' })
+  .then((res) => latest.value = res.data.length ? res.data[0] : undefined)
+  .catch((error) => console.error(error))
 </script>
