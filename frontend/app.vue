dbf196a8b331f890d76dc8d24fdc5f3e14017373
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-08-19 16:25:24 +0200

    feat: remove dotted background

--- frontend/app.vue
+++ frontend/app.vue
@@ -39,17 +39,6 @@
   filter: blur(5px);
   transform: translateY(5px);
 }
-
-body {
-  --dot-bg: white;
-  --dot-color: gray;
-  --dot-size: 1px;
-  --dot-space: 20px;
-  background:
-    linear-gradient(90deg, var(--dot-bg) calc(var(--dot-space) - var(--dot-size)), transparent 1%) center / var(--dot-space) var(--dot-space),
-    linear-gradient(var(--dot-bg) calc(var(--dot-space) - var(--dot-size)), transparent 1%) center / var(--dot-space) var(--dot-space),
-    var(--dot-color);
-}
 </style>
 
 <script setup>
