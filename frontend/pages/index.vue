6d9fe5226ad7a4997de054fe5ec7ebf788424896
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-08-19 18:32:45 +0200

    refactor: update landing page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,63 +1,28 @@
 <template>
-  <div>
-    <div class="flex justify-center">
-      <div class="max-w-md text-center flex flex-col items-center">
-        <NuxtLink to="/now" class="w-full">
-          <latest />
-        </NuxtLink>
-        <img src="/me.jpeg" alt="" class="min-w-40 mt-12 mb-6 h-40 w-40 rounded-full border-[5px]">
-        <BasePageTitle>Kasper JH Alfarnes</BasePageTitle>
-        <p class="text-md">Hi there! I'm a freelance software developer, web designer and soon to be CS
-          graduate.</p>
-      </div>
-    </div>
+  <div class="space-y-6">
+    <p>
+      I've been building and designing websites since 2019.
+      For me it's a great way of combining design and technology.
+      Over time I have gotten quite good at building with the Vue-ecosystem.
+    </p>
 
-    <section class="mt-32 space-y-2">
-      <BaseSubHeading :level="2">WHAT I DO</BaseSubHeading>
-      <p class="pb-4">
-        I'm interested in the intersection of design and technology.
-        Building for the web strikes a perfect balance, so I design and develop on freelance basis.
-      </p>
+    <p>
+      I take website commissions through my web devlopment agency Klosure.
+      We build high-end custom websites for businesses in creative industries.
+      If your looking for one, please visit
+      <NuxtLink to="https://klosure.dev">our website</NuxtLink> to schedule a call.
+    </p>
 
-      <BaseButtonGroup>
+    <p>
+      If you need help with an existing Vue project I would also be interested in working together on a freelance basis.
+      Visit my <NuxtLink to="/contact">contact page</NuxtLink>, to get in touch on LinkedIn.
 
-
-        <NuxtLink to="/contact">
-          <BaseButton variant="primary">
-            Work with me
-            <ArrowLongRightIcon class="size-5" />
-          </BaseButton>
-        </NuxtLink>
-
-        <NuxtLink to="/work">
-          <BaseButton variant="secondary">
-            My projects
-          </BaseButton>
-        </NuxtLink>
-      </BaseButtonGroup>
-    </section>
-
-    <section class="mt-32 space-y-2" v-if="newestPost">
-      <BaseSubHeading :level="2">RECENT WRITING</BaseSubHeading>
-      <p class="pb-6">
-        Once in a while, I write about technology, design and ongoing projects.
-        Here is my latest post:
-      </p>
-      <BlogPostCard :post="newestPost" />
-    </section>
-
-    <section class="mt-32 space-y-2">
-      <BaseSubHeading :level="2">STAY IN TOUCH</BaseSubHeading>
-      <p class="pb-8">
-        If you want to stay up to date on what im doing you can follow me here
-      </p>
-      <SocialsDisplay />
-    </section>
+    </p>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ArrowLongRightIcon } from '@heroicons/vue/20/solid';
+import { ref } from 'vue';
 import type { Post } from '~/types/Post'
 
 const strapi = useStrapi();
