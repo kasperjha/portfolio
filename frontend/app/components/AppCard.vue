100043ac01c72771e0609983585d630fb4fe3670
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-10-02 21:08:09 +0200

    refactor: extract app card component

---
+++ frontend/app/components/AppCard.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+interface Props {
+  label: string
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="rounded-xl border border-neutral-400 w-[330px] overflow-hidden">
+    <div class="border-b border-neutral-400 bg-neutral-100 h-9 flex justify-center items-center">
+      <p class="font-mono text-xs text-neutral-700">
+        {{ label }}
+      </p>
+    </div>
+    <slot />
+  </div>
+</template>
--- frontend/app/components/WorkItem/Website.vue
+++ frontend/app/components/WorkItem/Website.vue
@@ -9,12 +9,7 @@ interface Props {
 </script>
 
 <template>
-  <div class="rounded-xl border border-neutral-400 w-[330px] overflow-hidden">
-    <div class="border-b border-neutral-400 bg-neutral-100 h-9 flex justify-center items-center">
-      <p class="font-mono text-xs text-neutral-700">
-        {{ website.slug }}
-      </p>
-    </div>
-    <img class="w-full h-[630px] object-cover" :src="useMediaUrl(website.mockups.mobile.url)">
-  </div>
+  <AppCard :label="website.slug">
+    <img class="w-full h-[630px] object-cover" :src="useMediaUrl(website.mockups.mobile.url)"></img>
+  </AppCard>
 </template>
