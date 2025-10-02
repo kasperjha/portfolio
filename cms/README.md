04408a14e3945a39de579b75b7a4bbf4f05f2848
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-10-02 17:12:32 +0200

    refactor: wipe CMS

--- cms/README.md
+++
@@ -1,3 +0,0 @@
-# CMS
-
-TODO: write docs
--- cms/src/api/latest-update/services/latest-update.ts
+++
@@ -1,7 +0,0 @@
-/**
- * latest-update service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::latest-update.latest-update');
