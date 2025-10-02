8f09931d3cdbb3a353330c50cc341695e26236b8
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-10-02 15:55:19 +0200

    feat: add nav with avatar

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <TheNav />
   </div>
 </template>
---
+++ frontend/app/components/TheNav.vue
@@ -0,0 +1,17 @@
+<template>
+  <AppPadding class="py-10">
+    <nav class="flex">
+      <div class="flex items-center gap-5">
+        <img class="rounded-full size-14" src="/kasper-alfarnes-avatar.jpg">
+        <div class="hidden sm:block">
+          <p class="text-base">
+            Kasper Alfarnes
+          </p>
+          <p class="text-xs font-mono text-neutral-600">
+            Developer and designer
+          </p>
+        </div>
+      </div>
+    </nav>
+  </AppPadding>
+</template>
