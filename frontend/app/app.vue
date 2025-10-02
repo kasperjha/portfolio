d6f659f5d0abbd511f3b33aece4b8de89257c718
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-10-02 20:54:21 +0200

    feat: extract page component

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -8,31 +8,13 @@ useHead({
     { rel: 'manifest', href: '/favicon/site.webmanifest' },
   ],
 })
-
-// TODO: support preview feature
-const websites = ref()
-
-const options = {
-  populate: {
-    mockups: { populate: '*' },
-    about: { populate: '*' },
-    testemonial: { populate: '*' },
-  },
-}
-
-const strapi = useStrapi()
-strapi.find('websites', options)
-  .then(res => websites.value = res.data)
 </script>
 
 <template>
   <div>
     <TheNav />
-
     <main class="pb-30">
-      <AppPadding class="flex flex-wrap gap-5">
-        <WorkItemWebsite v-for="website in websites" :key="website.slug" :website="website" />
-      </AppPadding>
+      <NuxtPage />
     </main>
   </div>
 </template>
---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+// TODO: support preview feature
+const websites = ref()
+
+const options = {
+  populate: {
+    mockups: { populate: '*' },
+    about: { populate: '*' },
+    testemonial: { populate: '*' },
+  },
+}
+
+const strapi = useStrapi()
+strapi.find('websites', options)
+  .then(res => websites.value = res.data)
+</script>
+
+<template>
+  <AppPadding class="flex flex-wrap gap-5">
+    <WorkItemWebsite
+      v-for="website in websites"
+      :key="website.slug"
+      :website="website"
+      class="cursor-pointer"
+    />
+  </AppPadding>
+</template>
