d90280fa0e2a9b6a9ef2be827291188050cc61f4
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-10-04 01:15:24 +0200

    feat: set width and height for vertical mockup

--- frontend/app/components/Website/MockupCard.vue
+++ frontend/app/components/Website/MockupCard.vue
@@ -13,7 +13,17 @@ interface Props {
 
 <template>
   <AppCard :label="website.slug">
-    <img v-if="variant === 'vertical'" class="w-full h-[630px] object-cover" :src="useMediaUrl(website.mockups.mobile.url)">
-    <img v-if="variant === 'horizontal'" class="w-full object-cover" :src="useMediaUrl(website.mockups.desktop.url)">
+    <img
+      v-if="variant === 'vertical'"
+      class="w-full h-[630px] object-cover"
+      width="330px"
+      height="630px"
+      :src="useMediaUrl(website.mockups.mobile.url)"
+    >
+    <img
+      v-if="variant === 'horizontal'"
+      class="w-full object-cover"
+      :src="useMediaUrl(website.mockups.desktop.url)"
+    >
   </AppCard>
 </template>
