c743c9e670be86cda6bbe7a1189698956b2de62b
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-10-03 14:43:05 +0200

    feat: add toggle functionality

--- frontend/app/components/NavToggle.vue
+++ frontend/app/components/NavToggle.vue
@@ -1,14 +1,17 @@
+<script lang="ts" setup>
+const items = [
+  { label: 'all', to: '/' },
+  { label: 'web', to: '/web' },
+]
+
+const route = useRoute()
+</script>
+
 <template>
   <!-- TODO: implement working toggle -->
   <div class="flex bg-neutral-900 h-10 p-px rounded-full">
-    <button class="px-4 py-3 font-mono text-neutral-900 text-xs bg-white rounded-full">
-      all
-    </button>
-    <button class="px-4 py-3 font-mono text-white text-xs bg-neutral-900 rounded-full">
-      www
-    </button>
-    <button class="px-4 py-3 font-mono text-white text-xs bg-neutral-900 rounded-full">
-      txt
-    </button>
+    <NuxtLink v-for="item in items" :key="item.to" :to="item.to" class="block px-4 py-3 font-mono text-neutral-100 text-xs rounded-full transition" :class="{ 'bg-neutral-100 text-neutral-900': route.path === item.to }">
+      {{ item.label }}
+    </NuxtLink>
   </div>
 </template>
