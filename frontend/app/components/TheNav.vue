71caafb96ad3a6ee2b05ee3e9f31c8010c66b01c
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-10-03 15:00:11 +0200

    refactor: remove nav toggle for now

--- frontend/app/components/TheNav.vue
+++ frontend/app/components/TheNav.vue
@@ -1,20 +1,17 @@
 <template>
   <AppPadding class="py-10">
-    <nav class="flex items-center justify-between">
-      <div class="flex items-center gap-5">
-        <NuxtLink to="/">
-          <img class="rounded-full size-14" src="/kasper-alfarnes-avatar.jpg">
-        </NuxtLink>
-        <div class="hidden sm:block">
-          <p class="text-base">
-            Kasper Alfarnes
-          </p>
-          <p class="text-xs font-mono text-neutral-600">
-            Developer and designer
-          </p>
-        </div>
+    <nav class="flex items-center gap-5">
+      <NuxtLink to="/">
+        <img class="rounded-full size-14" src="/kasper-alfarnes-avatar.jpg">
+      </NuxtLink>
+      <div>
+        <p class="text-base">
+          Kasper Alfarnes
+        </p>
+        <p class="text-xs font-mono text-neutral-600">
+          Developer and designer
+        </p>
       </div>
-      <NavToggle />
     </nav>
   </AppPadding>
 </template>
