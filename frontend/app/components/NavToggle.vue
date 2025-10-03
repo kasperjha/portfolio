dcccb9fbd904423b0a68acf0545bc46fcfb79a37
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-10-03 14:21:07 +0200

    feat: use neutral color for toggle

--- frontend/app/components/NavToggle.vue
+++ frontend/app/components/NavToggle.vue
@@ -1,13 +1,13 @@
 <template>
   <!-- TODO: implement working toggle -->
-  <div class="flex bg-black h-10 p-px rounded-full">
-    <button class="px-4 py-3 font-mono text-black text-xs bg-white rounded-full">
+  <div class="flex bg-neutral-900 h-10 p-px rounded-full">
+    <button class="px-4 py-3 font-mono text-neutral-900 text-xs bg-white rounded-full">
       all
     </button>
-    <button class="px-4 py-3 font-mono text-white text-xs bg-black rounded-full">
+    <button class="px-4 py-3 font-mono text-white text-xs bg-neutral-900 rounded-full">
       www
     </button>
-    <button class="px-4 py-3 font-mono text-white text-xs bg-black rounded-full">
+    <button class="px-4 py-3 font-mono text-white text-xs bg-neutral-900 rounded-full">
       txt
     </button>
   </div>
