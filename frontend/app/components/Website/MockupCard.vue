693eecd12e08347abd108e0fcaf91c666810e3a2
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-10-04 02:09:05 +0200

    feat: add alt text to mockups

--- frontend/app/components/Website/MockupCard.vue
+++ frontend/app/components/Website/MockupCard.vue
@@ -17,12 +17,14 @@ interface Props {
       <img
         class="w-full h-[630px] object-cover"
         :src="useMediaUrl(website.mockups.mobile.url)"
+        :alt="`Screenshot of ${website.slug} on mobile`"
       >
     </div>
     <div v-if="variant === 'horizontal'" class="aspect-[512/315]">
       <img
         class="w-full object-cover"
         :src="useMediaUrl(website.mockups.desktop.url)"
+        :alt="`Screenshot of ${website.slug} on desktop`"
       >
     </div>
   </AppCard>
