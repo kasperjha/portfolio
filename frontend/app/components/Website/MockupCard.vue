5bc1edcca2aa54cc215efaa4c7f6fae707e941ae
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-10-04 14:51:36 +0200

    feat: use nuxt image for website mocks

--- frontend/app/components/Website/MockupCard.vue
+++ frontend/app/components/Website/MockupCard.vue
@@ -14,18 +14,24 @@ interface Props {
 <template>
   <AppCard :label="website.slug">
     <div v-if="variant === 'vertical'" class="aspect-[11/21]">
-      <img
+      <!-- assuming the verical mockup will not be shown larger than 330x630 px -->
+      <NuxtImg
         class="w-full h-[630px] object-cover"
+        width="330px"
+        height="630px"
         :src="useMediaUrl(website.mockups.mobile.url)"
         :alt="`Screenshot of ${website.slug} on mobile`"
-      >
+      />
     </div>
+    <!-- assuming horizontal mockup will not be shown larger than 557x343 px -->
     <div v-if="variant === 'horizontal'" class="aspect-[512/315]">
-      <img
+      <NuxtImg
         class="w-full object-cover"
         :src="useMediaUrl(website.mockups.desktop.url)"
+        width="557"
+        height="343"
         :alt="`Screenshot of ${website.slug} on desktop`"
-      >
+      />
     </div>
   </AppCard>
 </template>
