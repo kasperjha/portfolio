4de1382833a66c0f01c810026783c3b234e12ae4
Kasper Johannes Hopen Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-10-07 16:31:58 +0200

    Shortlinks (#17)
    This PR creates a shortlink route with cloudflare pages
    - Shortlinks URLs have format like `/s/<uuid>`
    - Visiting shortlink url will cause a 302 (temporary) redirect
    - UUIDs and destination URLs are stored in cloudflare KV

---
+++ frontend/functions/s/[uuid].js
@@ -0,0 +1,14 @@
+/**
+ * Handles shortlink redirects to `/s/<uuid>`.
+ * Shortlinks are defined in Cloudflare KV.
+ */
+export async function onRequest(context) {
+  const { uuid } = context.params
+  const links = context.env.SHORTLINKS
+
+  const url = await links.get(uuid)
+
+  return url
+    ? Response.redirect(url, 302)
+    : new Response('Link not found', { status: 404 })
+}
