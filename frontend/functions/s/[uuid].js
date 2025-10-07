45cbcd824b2cfac0d10eaf1bdf1903a39f7c2f05
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-10-07 15:27:47 +0200

    feat: test uuid based redirect

--- frontend/functions/s/[uuid].js
+++ frontend/functions/s/[uuid].js
@@ -1,3 +1,9 @@
 export function onRequest(context) {
-  return new Response('Hello, world!')
+  const { uuid } = context.params
+
+  if (uuid === 'abc123') {
+    return Response.redirect('https://alfarnes.dev/web/lu20n4q82b0ffnx7jlggv778', 301)
+  }
+
+  return new Response('Link not found', { status: 404 })
 }
