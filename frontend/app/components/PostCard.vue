b77d0cc925d8eda6759907c39426d5306b312440
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-10-19 15:26:20 +0200

    feat: implement mocked post card

---
+++ frontend/app/components/PostCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <article class="rounded-xl border border-neutral-400 bg-neutral-100">
+    <div class="border-4 border-neutral-50 px-7 space-y-5 py-8 rounded-xl h-full shadow-md flex flex-col">
+      <h2 class="text-xl text-neutral-800 leading-8">
+        Three gotchas with Nuxt's Image module
+      </h2>
+      <p class="leading-7 line-clamp-4">
+        It is not easy to design products that appeal to everyone.
+        But we can still have designs that appeal to “most” people.
+        Another line that should be clamped away.
+      </p>
+      <div class="font-mono flex justify-between mt-auto text-sm">
+        <p>02 MAR, 24</p>
+        <p>21 min read</p>
+      </div>
+    </div>
+  </article>
+</template>
