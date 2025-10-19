e9cf444a9b29db38af26e908a7a0cca6fd93fe9e
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-10-19 15:48:09 +0200

    feat: use actual title and description

--- frontend/app/components/PostCard.vue
+++ frontend/app/components/PostCard.vue
@@ -1,13 +1,20 @@
+<script lang="ts" setup>
+import type { Post } from '~/types/cms/collections/Post'
+
+interface Props {
+  post: Post
+}
+const props = defineProps<Props>()
+</script>
+
 <template>
   <article class="rounded-xl border border-neutral-400 bg-neutral-100">
     <div class="border-4 border-neutral-50 px-7 space-y-5 py-8 rounded-xl h-full shadow-md flex flex-col">
       <h2 class="text-xl text-neutral-800 leading-8">
-        Three gotchas with Nuxt's Image module
+        {{ post.title }}
       </h2>
       <p class="leading-7 line-clamp-4">
-        It is not easy to design products that appeal to everyone.
-        But we can still have designs that appeal to “most” people.
-        Another line that should be clamped away.
+        {{ post.description }}
       </p>
       <div class="font-mono flex justify-between mt-auto text-sm">
         <p>02 MAR, 24</p>
