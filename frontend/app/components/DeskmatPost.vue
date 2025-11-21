d602a8855ba71bc59cff91907e19f44b79699e5f
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-11-21 01:41:04 +0100

    feat: create deskmat grid layout

---
+++ frontend/app/components/DeskmatPost.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import type { Post } from '~/types/cms/collections/Post'
+import { buildPostUrl } from '~/modules/utility/urls'
+
+interface Props {
+  post: Post
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <NuxtLink :to="buildPostUrl(post)">
+    <PostCard :post class="row-span-1 h-full" />
+  </NuxtLink>
+</template>
