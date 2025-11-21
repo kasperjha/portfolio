a9a04d9fb3dbccb425e36ac8095f21c133349e48
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-11-21 14:15:42 +0100

    feat: display blogposts on landing page

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,28 +1,35 @@
 <script lang="ts" setup>
+import type { Post } from '~/types/cms/collections/Post'
+import type { Website } from '~/types/cms/collections/Website'
+import { getDeskmatItems, sortDeskmatItems } from '~/modules/utility/deskmat'
 
 // TODO: support preview feature
-const websites = ref()
-
-const options = {
-  populate: {
-    mockups: { populate: '*' },
-    about: { populate: '*' },
-    testimonial: { populate: '*' },
-  },
-  sort: ['actuallyPublishedAt:desc', 'publishedAt'],
-}
-
 const strapi = useStrapi()
-await strapi.find('websites', options)
+
+const websites = ref<Website[]>()
+await strapi.find<Website>('websites', { populate: { mockups: { populate: '*' } } })
   .then(res => websites.value = res.data)
 
 useBreadcrumbs([{ label: 'home', to: '/' }])
+const posts = ref<Post[]>()
+await strapi.find<Post>('posts')
+  .then(res => posts.value = res.data)
+
+const deskmatItems = computed(() =>
+  sortDeskmatItems(getDeskmatItems(
+    websites.value as Website[],
+    posts.value as Post[],
+  )),
+)
 </script>
 
 <template>
   <AppPadding>
     <DeskmatGrid>
-      <DeskmatWebsite v-for="website in websites" :key="website.slug" :website />
+      <template v-for="{ key, item } in deskmatItems" :key="item.slug">
+        <DeskmatWebsite v-if="key === 'website'" :website="(item as Website)" />
+        <DeskmatPost v-else :post="(item as Post)" />
+      </template>
     </DeskmatGrid>
   </AppPadding>
 </template>
