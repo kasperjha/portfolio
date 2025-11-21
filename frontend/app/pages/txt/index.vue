c3f7da7ba159541bb60f70bb237214b629835251
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-11-21 02:19:39 +0100

    feat: add post overview page

---
+++ frontend/app/pages/txt/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const posts = ref()
+const strapi = useStrapi()
+await strapi.find('posts')
+  .then(res => posts.value = res.data)
+</script>
+
+<template>
+  <AppPadding>
+    <DeskmatGrid>
+      <DeskmatPost v-for="post in posts" :key="post.slug" :post />
+    </DeskmatGrid>
+  </AppPadding>
+</template>
