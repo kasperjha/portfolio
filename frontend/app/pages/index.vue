3f7218b4166b8261fa7a535cb4ef15a711e62824
Kasper Johannes Hopen Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-11-30 14:07:16 +0100

    Fully static builds (#21)
    This PR introduces fully static builds
    - Used `useAsyncData` and `experimental.payloadExtraction` to enable fully static builds
    - Specified node version to `v22` for CMS
    
    Also discovered this: `nuxt/image` only caches images locally if they are served from a non `localhost` URL

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -3,23 +3,14 @@ import type { Post } from '~/types/cms/collections/Post'
 import type { Website } from '~/types/cms/collections/Website'
 import { getDeskmatItems, sortDeskmatItems } from '~/modules/utility/deskmat'
 
-// TODO: support preview feature
-const strapi = useStrapi()
-
-const websites = ref<Website[]>()
-await strapi.find<Website>('websites', { populate: { mockups: { populate: '*' } } })
-  .then(res => websites.value = res.data)
-
-const posts = ref<Post[]>()
-await strapi.find<Post>('posts')
-  .then(res => posts.value = res.data)
-
-const deskmatItems = computed(() =>
-  sortDeskmatItems(getDeskmatItems(
-    websites.value as Website[],
-    posts.value as Post[],
-  )),
-)
+const { data: deskmatItems } = await useAsyncData(async () => {
+  const strapi = useStrapi()
+  const [websites, posts] = await Promise.all([
+    strapi.find<Website>('websites', { populate: { mockups: { populate: '*' } } }),
+    strapi.find<Post>('posts'),
+  ])
+  return sortDeskmatItems(getDeskmatItems(websites.data, posts.data))
+})
 
 useBreadcrumbs([{ label: 'home' }])
 </script>
