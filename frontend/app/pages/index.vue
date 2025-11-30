e953398928c988e2768bf3ce61df303fc3f076db
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-11-30 13:53:17 +0100

    feat: compute deskmat items in async call

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -3,20 +3,12 @@ import type { Post } from '~/types/cms/collections/Post'
 import type { Website } from '~/types/cms/collections/Website'
 import { getDeskmatItems, sortDeskmatItems } from '~/modules/utility/deskmat'
 
-const { data } = await useAsyncData(async () => {
+const { data: deskmatItems } = await useAsyncData(async () => {
   const strapi = useStrapi()
   const [websites, posts] = await Promise.all([
     strapi.find<Website>('websites', { populate: { mockups: { populate: '*' } } }),
     strapi.find<Post>('posts'),
   ])
-  return { websites, posts }
-})
-
-const deskmatItems = computed(() => {
-  if (!data.value) {
-    return []
-  }
-  const { posts, websites } = data.value
   return sortDeskmatItems(getDeskmatItems(websites.data, posts.data))
 })
 
