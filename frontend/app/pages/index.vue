kasperjha/portfolio

commit 3f7218b4166b8261fa7a535cb4ef15a711e62824

Fully static builds (#21)
This PR introduces fully static builds
- Used `useAsyncData` and `experimental.payloadExtraction` to enable fully static builds
- Specified node version to `v22` for CMS

Also discovered this: `nuxt/image` only caches images locally if they are served from a non `localhost` URL

cms/.node-version
@@ -1,0 +1,1 @@
+v22

frontend/app/pages/index.vue
@@ -3,23 +3,14 @@ import type { Post } from '~/types/cms/collections/Post'
 import type { Website } from '~/types/cms/collections/Website'
 import { getDeskmatItems, sortDeskmatItems } from '~/modules/utility/deskmat'
 
-// TODO: support preview feature
-const strapi = useStrapi()
-
-const websites = ref<Website[]>()
-await strapi.find<Website>('websites', { populate: { mockups: { populate: '*' } } })
-  .then(res => websites.value = res.data)
-
-const posts = ref<Post[]>()
-await strapi.find<Post>('posts')
-  .then(res => posts.value = res.data)
-
-const deskmatItems = computed(() =>
-  sortDeskmatItems(getDeskmatItems(
-    websites.value as Website[],
-    posts.value as Post[],
-  )),
-)
+const { data: deskmatItems } = await useAsyncData(async () => {
+  const strapi = useStrapi()
+  const [websites, posts] = await Promise.all([
+    strapi.find<Website>('websites', { populate: { mockups: { populate: '*' } } }),
+    strapi.find<Post>('posts'),
+  ])
+  return sortDeskmatItems(getDeskmatItems(websites.data, posts.data))
+})
 
 useBreadcrumbs([{ label: 'home' }])
 </script>

frontend/app/pages/txt/[id].vue
@@ -1,11 +1,12 @@
 <script lang="ts" setup>
 import type { Post } from '~/types/cms/collections/Post'
 
-const post = ref<Post>()
 const route = useRoute()
+
 const strapi = useStrapi()
-await strapi.findOne<Post>('posts', route.params.id as string)
-  .then(res => post.value = res.data)
+const { data: post } = await useAsyncData(async () =>
+  (await strapi.findOne<Post>('posts', route.params.id as string)).data,
+)
 
 useBreadcrumbs([
   { label: 'home', to: '/' },

frontend/app/pages/txt/index.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import type { Post } from '~/types/cms/collections/Post'
 
-const posts = ref<Post[]>()
 const strapi = useStrapi()
-await strapi.find<Post>('posts')
-  .then(res => posts.value = res.data)
+const { data: posts } = await useAsyncData(
+  async () => (await strapi.find<Post>('posts')).data,
+)
 
 useBreadcrumbs([
   { label: 'home', to: '/' },

frontend/app/pages/web/[id].vue
@@ -3,8 +3,6 @@ import type { Website } from '~/types/cms/collections/Website'
 
 const route = useRoute()
 
-const website = ref()
-
 const options = {
   populate: {
     mockups: { populate: '*' },
@@ -16,8 +14,9 @@ const options = {
 }
 
 const strapi = useStrapi()
-await strapi.findOne('websites', route.params.id as string, options)
-  .then(res => website.value = res.data)
+const { data: website } = await useAsyncData(async () =>
+  (await strapi.findOne<Website>('websites', route.params.id as string, options)).data,
+)
 
 /**
  * Creates URL to og-image for website project.
@@ -30,17 +29,17 @@ function getOgImage(website: Website) {
 }
 
 useSeoMeta({
-  title: `Project: ${website.value.about.title}`,
-  ogTitle: `Project: ${website.value.about.title}`,
-  description: website.value.about.ingress,
-  ogDescription: website.value.about.ingress,
-  ogImage: getOgImage(website.value),
+  title: `Project: ${website.value?.about.title}`,
+  ogTitle: `Project: ${website.value?.about.title}`,
+  description: website.value?.about.ingress,
+  ogDescription: website.value?.about.ingress,
+  // ogImage: getOgImage(website.value),
 })
 
 useBreadcrumbs([
   { label: 'home', to: '/' },
   { label: 'web', to: '/web' },
-  { label: website.value.slug },
+  { label: website.value?.slug },
 ])
 </script>
 

frontend/app/pages/web/index.vue
@@ -1,9 +1,6 @@
 <script lang="ts" setup>
 import type { Website } from '~/types/cms/collections/Website'
 
-// TODO: support preview feature
-const websites = ref<Website[]>()
-
 const options = {
   populate: {
     mockups: { populate: '*' },
@@ -14,8 +11,9 @@ const options = {
 }
 
 const strapi = useStrapi()
-await strapi.find<Website>('websites', options)
-  .then(res => websites.value = res.data)
+const { data: websites } = await useAsyncData(
+  async () => (await strapi.find<Website>('websites', options)).data,
+)
 
 useSeoMeta({
   title: 'Websites',

frontend/nuxt.config.ts
@@ -16,6 +16,10 @@ export default defineNuxtConfig({
     plugins: [tailwindcss()],
   },
 
+  experimental: {
+    payloadExtraction: true,
+  },
+
   image: {
     domains: [new URL(import.meta.env.VITE_PUBLIC_CMS_URL).hostname],
     format: ['avif', 'webp'],

commit 9d438bc5ddb1857b67ef9cb5cad67e69d6802aac

Fix static builds (#22)
This PR fixes an issue introduced in #21 
- `/web/<id>` and `/txt/<id>` pages were missing a unique fetch key
- This resulted in the duplicated data across pages 
- This PR introduces the missing fetch keys 
- Ensuring correct data for each web and txt page

cms/.node-version
@@ -1,0 +1,1 @@
+v22

frontend/app/pages/index.vue
@@ -3,23 +3,14 @@ import type { Post } from '~/types/cms/collections/Post'
 import type { Website } from '~/types/cms/collections/Website'
 import { getDeskmatItems, sortDeskmatItems } from '~/modules/utility/deskmat'
 
-// TODO: support preview feature
-const strapi = useStrapi()
-
-const websites = ref<Website[]>()
-await strapi.find<Website>('websites', { populate: { mockups: { populate: '*' } } })
-  .then(res => websites.value = res.data)
-
-const posts = ref<Post[]>()
-await strapi.find<Post>('posts')
-  .then(res => posts.value = res.data)
-
-const deskmatItems = computed(() =>
-  sortDeskmatItems(getDeskmatItems(
-    websites.value as Website[],
-    posts.value as Post[],
-  )),
-)
+const { data: deskmatItems } = await useAsyncData(async () => {
+  const strapi = useStrapi()
+  const [websites, posts] = await Promise.all([
+    strapi.find<Website>('websites', { populate: { mockups: { populate: '*' } } }),
+    strapi.find<Post>('posts'),
+  ])
+  return sortDeskmatItems(getDeskmatItems(websites.data, posts.data))
+})
 
 useBreadcrumbs([{ label: 'home' }])
 </script>

frontend/app/pages/txt/[id].vue
@@ -1,11 +1,11 @@
 <script lang="ts" setup>
 import type { Post } from '~/types/cms/collections/Post'
 
-const post = ref<Post>()
 const route = useRoute()
+
 const strapi = useStrapi()
-await strapi.findOne<Post>('posts', route.params.id as string)
-  .then(res => post.value = res.data)
+const { data: post } = await useAsyncData(`txt-${route.params.id}`, async () =>
+  (await strapi.findOne<Post>('posts', route.params.id as string)).data)
 
 useBreadcrumbs([
   { label: 'home', to: '/' },

frontend/app/pages/txt/index.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import type { Post } from '~/types/cms/collections/Post'
 
-const posts = ref<Post[]>()
 const strapi = useStrapi()
-await strapi.find<Post>('posts')
-  .then(res => posts.value = res.data)
+const { data: posts } = await useAsyncData(
+  async () => (await strapi.find<Post>('posts')).data,
+)
 
 useBreadcrumbs([
   { label: 'home', to: '/' },

frontend/app/pages/web/[id].vue
@@ -3,8 +3,6 @@ import type { Website } from '~/types/cms/collections/Website'
 
 const route = useRoute()
 
-const website = ref()
-
 const options = {
   populate: {
     mockups: { populate: '*' },
@@ -16,8 +14,8 @@ const options = {
 }
 
 const strapi = useStrapi()
-await strapi.findOne('websites', route.params.id as string, options)
-  .then(res => website.value = res.data)
+const { data: website } = await useAsyncData(`web-${route.params.id}`, async () =>
+  (await strapi.findOne<Website>('websites', route.params.id as string, options)).data)
 
 /**
  * Creates URL to og-image for website project.
@@ -30,17 +28,17 @@ function getOgImage(website: Website) {
 }
 
 useSeoMeta({
-  title: `Project: ${website.value.about.title}`,
-  ogTitle: `Project: ${website.value.about.title}`,
-  description: website.value.about.ingress,
-  ogDescription: website.value.about.ingress,
-  ogImage: getOgImage(website.value),
+  title: `Project: ${website.value?.about.title}`,
+  ogTitle: `Project: ${website.value?.about.title}`,
+  description: website.value?.about.ingress,
+  ogDescription: website.value?.about.ingress,
+  // ogImage: getOgImage(website.value),
 })
 
 useBreadcrumbs([
   { label: 'home', to: '/' },
   { label: 'web', to: '/web' },
-  { label: website.value.slug },
+  { label: website.value?.slug },
 ])
 </script>
 

frontend/app/pages/web/index.vue
@@ -1,9 +1,6 @@
 <script lang="ts" setup>
 import type { Website } from '~/types/cms/collections/Website'
 
-// TODO: support preview feature
-const websites = ref<Website[]>()
-
 const options = {
   populate: {
     mockups: { populate: '*' },
@@ -14,8 +11,9 @@ const options = {
 }
 
 const strapi = useStrapi()
-await strapi.find<Website>('websites', options)
-  .then(res => websites.value = res.data)
+const { data: websites } = await useAsyncData(
+  async () => (await strapi.find<Website>('websites', options)).data,
+)
 
 useSeoMeta({
   title: 'Websites',

frontend/nuxt.config.ts
@@ -16,6 +16,10 @@ export default defineNuxtConfig({
     plugins: [tailwindcss()],
   },
 
+  experimental: {
+    payloadExtraction: true,
+  },
+
   image: {
     domains: [new URL(import.meta.env.VITE_PUBLIC_CMS_URL).hostname],
     format: ['avif', 'webp'],