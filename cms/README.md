04f5c8a87267903cbb0d86f15145536dadf36332
Kasper Johannes Hopen Alfarnes <49387825+kasperjha@users.noreply.github.com>
2026-03-01 15:18:05 +0100

    CI for building and pushing CMS docker image (#24)
    This PR introduces CI to build and push CMS docker image
    
    - Added `build.yml`
    - Fixed broken readme links
    - Disabled strapi telemetry
    - Disabled strapi cloud plugin
    - Upgraded strapi version
    - Added package script for config sync

--- cms/README.md
+++ cms/README.md
@@ -1,5 +1,7 @@
 # CMS
 
+The CMS is built with Strapi.
+
 ## Node
 
 With node version manager `n` you can auto install the appropriate version of node by running `n auto`.
--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -1,1 +1,15 @@
-export default () => ({});
+export default () => ({
+  'config-sync': {
+    enabled: true,
+    config: {
+      importOnBootstrap: true,
+      excludedConfig: [
+        'core-store.plugin_users-permissions_grant',
+        'core-store.plugin_upload_metrics',
+        'core-store.strapi_content_types_schema',
+        'core-store.ee_information',
+        'core-store.strapi_unidirectional-join-table-repair-ran'
+      ]
+    }
+  }
+});
