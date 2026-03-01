ca04a02ec4698e538e9ffb6ff6fbe41d31c6af69
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2026-03-01 14:52:55 +0100

    feat: configure auto sync and exclude

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -1,1 +1,12 @@
-export default () => ({});
+export default () => ({
+  'config-sync': {
+    importOnBootstrap: true,
+    excludedConfig: [
+      'core-store.plugin_users-permissions_grant',
+      'core-store.plugin_upload_metrics',
+      'core-store.strapi_content_types_schema',
+      'core-store.ee_information',
+      'strapi_unidirectional-join-table-repair-ran'
+    ]
+  }
+});
