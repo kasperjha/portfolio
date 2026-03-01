30a4bf9754222b25f59f80a40d4d6779240a6d0a
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2026-03-01 14:59:01 +0100

    fix: incorrect config format

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -1,12 +1,15 @@
 export default () => ({
   'config-sync': {
-    importOnBootstrap: true,
-    excludedConfig: [
-      'core-store.plugin_users-permissions_grant',
-      'core-store.plugin_upload_metrics',
-      'core-store.strapi_content_types_schema',
-      'core-store.ee_information',
-      'strapi_unidirectional-join-table-repair-ran'
-    ]
+    enabled: true,
+    config: {
+      importOnBootstrap: true,
+      excludedConfig: [
+        'core-store.plugin_users-permissions_grant',
+        'core-store.plugin_upload_metrics',
+        'core-store.strapi_content_types_schema',
+        'core-store.ee_information',
+        'core-store.strapi_unidirectional-join-table-repair-ran'
+      ]
+    }
   }
 });
